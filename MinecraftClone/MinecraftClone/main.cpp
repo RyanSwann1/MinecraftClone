8dbf596db3ea9f01c54ee57aaefa17540b9b5f75
Ryan Swann <RyanSwann1992@gmail.com>
2020-01-10 22:13:11 +0000

    Project Setup

--- MinecraftClone/MinecraftClone/main.cpp
+++ MinecraftClone/MinecraftClone/main.cpp
@@ -1,10 +1,12 @@
 
 #include <string>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow window;
+
 	std::string gameName("MineCraft");
 	std::cout << gameName << "\n";
-
 }
