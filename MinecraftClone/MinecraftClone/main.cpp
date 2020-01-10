1c9f4455105cd3c489a66ed4f8c7235f8bc86a2e
Ryan Swann <RyanSwann1992@gmail.com>
2020-01-10 22:35:20 +0000

    Added more critical project files

--- MinecraftClone/MinecraftClone/main.cpp
+++ MinecraftClone/MinecraftClone/main.cpp
@@ -2,11 +2,31 @@
 #include <string>
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "glad.h"
 
 int main()
 {
-	sf::RenderWindow window;
+	sf::RenderWindow window(sf::VideoMode(750, 750), "Minecraft", sf::Style::Default);
 
 	std::string gameName("MineCraft");
 	std::cout << gameName << "\n";
+	
+	while (window.isOpen())
+	{
+		gladLoadGL();
+		sf::Event sfmlEvent;
+		if (window.pollEvent(sfmlEvent))
+		{
+			if (sfmlEvent.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//window.clear(sf::Color::Black);
+		window.display();
+	}
 }
