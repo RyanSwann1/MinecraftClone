76f0c5b2ad7d215c9a24056f9de13dbcf54631da
Ryan Swann <RyanSwann1992@gmail.com>
2020-02-29 20:46:49 +0000

    Removed unneeded code

--- MinecraftClone/MinecraftClone/main.cpp
+++ MinecraftClone/MinecraftClone/main.cpp
@@ -243,8 +243,7 @@ int main()
 
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClear(GL_DEPTH_BUFFER_BIT);
-		std::cout << glGetError() << "\n";
-		std::cout << glGetError() << "\n";
+
 		glm::mat4 view = glm::mat4(1.0f);
 		view = glm::lookAt(camera.m_position, camera.m_position + camera.m_front, camera.m_up);
 		setUniformMat4f(shaderID, "uView", view, uniformLocations);
@@ -275,7 +274,6 @@ int main()
 			{
 				VAO.second.bindOpaqueVAO();
 				glDrawElements(GL_TRIANGLES, VAO.second.m_vertexBuffer.indicies.size(), GL_UNSIGNED_INT, nullptr);
-				VAO.second.unbind();
 			}
 		}
 
@@ -294,7 +292,6 @@ int main()
 			{
 				VAO.second.bindTransparentVAO();
 				glDrawElements(GL_TRIANGLES, VAO.second.m_vertexBuffer.transparentIndicies.size(), GL_UNSIGNED_INT, nullptr);
-				VAO.second.unbind();
 			}
 		}
 		setUniformLocation1f(shaderID, "uAlpha", 1.0f, uniformLocations);
