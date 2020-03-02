7c3e5389fba252465609117cfb0fb8a7e4245b24
Ryan Swann <RyanSwann1992@gmail.com>
2020-03-02 22:34:09 +0000

    Stashed

--- MinecraftClone/MinecraftClone/Chunk.h
+++ MinecraftClone/MinecraftClone/Chunk.h
@@ -30,14 +30,30 @@ public:
 	const glm::ivec3& getStartingPosition() const;
 	const CubeDetails& getCubeDetailsWithoutBoundsCheck(const glm::ivec3& position) const;
 
+	Chunk* getNext();
+	void setNext(Chunk* chunk);
 	void reset(const glm::ivec3& startingPosition);
 	void release();
 
 private:
 	bool m_inUse;
-	glm::ivec3 m_startingPosition;
-	glm::ivec3 m_endingPosition;
-	std::array<std::array<std::array<CubeDetails, Utilities::CHUNK_DEPTH>, Utilities::CHUNK_HEIGHT>, Utilities::CHUNK_WIDTH> m_chunk;
-	
+	union State
+	{
+		State()
+			: m_startingPosition(),
+			m_endingPosition(),
+			m_chunk()
+		{}
+
+		struct
+		{
+			glm::ivec3 m_startingPosition;
+			glm::ivec3 m_endingPosition;
+			std::array<std::array<std::array<CubeDetails, Utilities::CHUNK_DEPTH>, Utilities::CHUNK_HEIGHT>, Utilities::CHUNK_WIDTH> m_chunk;
+		};
+
+		Chunk* m_next = nullptr;
+	} m_state;
+
 	void regen(const glm::ivec3& startingPosition);
 };
--- MinecraftClone/MinecraftClone/ChunkPool.cpp
+++ MinecraftClone/MinecraftClone/ChunkPool.cpp
@@ -3,10 +3,24 @@
 ChunkPool::ChunkPool()
 {
 	m_chunks.resize(size_t(2500));
+	
+	m_firstAvailable = &m_chunks.front();
+
+	for (int i = 0; i < m_chunks.size() - 1; ++i)
+	{
+		m_chunks[i].setNext(&m_chunks[i + 1]);
+	}
+
+	m_chunks.back().setNext(nullptr);
 }
 
 Chunk& ChunkPool::getChunk(const glm::ivec3& startingPosition)
 {
+	//Chunk* chunk = m_firstAvailable;
+	//m_firstAvailable = chunk->getNext();
+
+	//return *chunk;
+
 	for (auto& chunk : m_chunks)
 	{
 		if (!chunk.isInUse())
@@ -15,4 +29,10 @@ Chunk& ChunkPool::getChunk(const glm::ivec3& startingPosition)
 			return chunk;
 		}
 	}
+}
+
+void ChunkPool::releaseChunk(Chunk& chunk)
+{
+	chunk.setNext(m_firstAvailable);
+	m_firstAvailable = &chunk;
 }
