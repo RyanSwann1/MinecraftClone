2cf67efdb75ff1f69b0b2939c1a12e38002656ad
Ryan Swann <RyanSwann1992@gmail.com>
2020-03-11 15:11:30 +0000

    Removed NonMovable

--- MinecraftClone/MinecraftClone/NonCopyable.h
+++ MinecraftClone/MinecraftClone/NonCopyable.h
@@ -6,6 +6,4 @@ public:
 	NonCopyable() {}
 	NonCopyable(const NonCopyable&) = delete;
 	NonCopyable& operator=(const NonCopyable&) = delete;
-	NonCopyable(NonCopyable&&) = delete;
-	NonCopyable&& operator=(NonCopyable&&) = delete;
 };
