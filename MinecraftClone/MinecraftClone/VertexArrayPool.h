120cea973e768ef0f52d8fc1087aac5edd9cd4a0
Ryan Swann <RyanSwann1992@gmail.com>
2020-03-16 22:04:14 +0000

    Removed uneeded comments

--- MinecraftClone/MinecraftClone/VertexArrayPool.h
+++ MinecraftClone/MinecraftClone/VertexArrayPool.h
@@ -3,27 +3,6 @@
 #include "VertexArray.h"
 #include "ObjectPool.h"
 
-//template <class T>
-//class ObjectPool : private NonCopyable, private NonMovable
-//{
-//protected:
-//	std::vector<T> m_objectPool;
-//	T* m_nextAvailable;
-//};
-//
-//class VertexArrayPool : private ObjectPool<VertexArray>
-//{
-//public:
-//	VertexArray& getVertexArray();
-//};
-//
-//class Chunk;
-//class ChunkPool : private ObjectPool<Chunk>
-//{
-//public:
-//	Chunk& getChunk(const glm::ivec3& startingPosition);
-//};
-
 class VertexArrayPool : private ObjectPool<VertexArray>
 {
 public:
