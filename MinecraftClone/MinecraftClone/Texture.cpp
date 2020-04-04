e37d7a880bc54a3219d3edf6e58e7cccd7365eed
Ryan Swann <RyanSwann1992@gmail.com>
2020-04-04 21:30:25 +0100

    Textures now displaying correctly
    Bug fixed, textures no longer upside down

--- MinecraftClone/MinecraftClone/Texture.cpp
+++ MinecraftClone/MinecraftClone/Texture.cpp
@@ -4,9 +4,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Utilities.h"
-#include <mutex>
 
-//Texture Array
 TextureArray::TextureArray(unsigned int slot)
 	: m_textureSize(16, 16),
 	m_slot(slot),
@@ -40,7 +38,7 @@ bool TextureArray::addTexture(const std::string& textureName)
 		std::cout << textureName << " not loaded.\n";
 		return false;
 	}
-
+	image.flipVertically();
 	glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, m_textureCount, m_textureSize.x, m_textureSize.y, 
 		1, GL_RGBA, GL_UNSIGNED_BYTE, image.getPixelsPtr());
 	++m_textureCount;
@@ -56,58 +54,3 @@ void TextureArray::bind() const
 {
 	glBindTexture(GL_TEXTURE_2D_ARRAY, m_ID);
 }
-
-//Texture
-//Texture::Texture()
-//	: m_currentSlot(0),
-//	m_ID(0)
-//{}
-//
-//Texture::~Texture()
-//{
-//	assert(m_ID != Utilities::INVALID_OPENGL_ID);
-//	glDeleteTextures(1, &m_ID);
-//}
-//
-//std::unique_ptr<Texture> Texture::loadTexture(const std::string& name)
-//{
-//	std::unique_ptr<Texture> texture(new Texture());
-//	sf::Image image;
-//	if (!image.loadFromFile(name))
-//	{
-//		return std::unique_ptr<Texture>();
-//	}
-//	image.flipVertically();
-//
-//	glGenTextures(1, &texture->m_ID);
-//	glBindTexture(GL_TEXTURE_2D_ARRAY, texture->m_ID);
-//
-//	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-//	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//	
-//	glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGB, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.getSize().x, image.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getPixelsPtr());
-//	glGenerateMipmap(GL_TEXTURE_2D);
-//	glBindTexture(GL_TEXTURE_2D, 0);
-//
-//	return texture;
-//}
-//
-//unsigned int Texture::getCurrentSlot() const
-//{
-//	return m_currentSlot;
-//}
-//
-//void Texture::bind(unsigned int slot)
-//{
-//	m_currentSlot = slot;
-//	glActiveTexture(GL_TEXTURE0 + m_currentSlot);
-//	glBindTexture(GL_TEXTURE_2D, m_ID);
-//}
-//
-//void Texture::unbind() const
-//{
-//	glBindTexture(GL_TEXTURE_2D, 0);
-//}
