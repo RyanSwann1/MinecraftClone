3a838f4055e5935e4254cea486d240a251b2b610
Ryan Swann <RyanSwann1992@gmail.com>
2020-04-05 22:33:56 +0100

    Changed Shader file type & implemented simple lighting
    Lighting is based off of singular float value. Allows me to control light intensity per cube face

--- MinecraftClone/MinecraftClone/FragmentShader.glsl
+++ MinecraftClone/MinecraftClone/FragmentShader.glsl
@@ -4,14 +4,13 @@ out vec4 color;
 uniform sampler2DArray uTexture;
 
 in vec3 vTextCoord;
+in float vlightIntensity;
 
 void main()
 {
-	vec4 texColor = texture(uTexture, vTextCoord);
-	if (texColor.a < 0.1)
+	color = vlightIntensity * texture(uTexture, vTextCoord);
+	if (color.a == 0)
 	{
 		discard;
 	}
-
-	color = texColor;
 };
--- MinecraftClone/MinecraftClone/VertexShader.glsl
+++ MinecraftClone/MinecraftClone/VertexShader.glsl
@@ -2,15 +2,17 @@
 
 layout(location = 0) in vec3 aPos; 
 layout(location = 1) in vec3 textCoord;
+layout(location = 2) in float lightIntensity;
 
-uniform mat4 uModel;
 uniform mat4 uView;
 uniform mat4 uProjection;
 
 out vec3 vTextCoord;
+out float vlightIntensity;
 
 void main()
 {
 	gl_Position = uProjection * uView * vec4(aPos, 1.0);
 	vTextCoord = textCoord;
+	vlightIntensity = lightIntensity;
 }
