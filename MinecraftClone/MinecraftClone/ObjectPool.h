9ea602f333e631babd666c02bb3393817e920e94
Ryan Swann <RyanSwann1992@gmail.com>
2020-04-06 16:04:35 +0100

    Removed linked list functionality and return type of 'getNextAvailableObject'
    Removed linked list functionality because it wasn't being used in the normal sense. It is meant to be used to garuntee O(1) access but I was using it in O(n) access. Also it allowed for more potential errors such as keeping the linked list connected properly.
    
    Also fixed issue where while loop wouldn't potentially stop execution on release mode due to the assert not being hit
    
    The return type was changed because it couldn't always garuntee to retreieve a valid object that wasn't in use.
    Now returning a pointer allows me to check if its null.  If null then don't interact with the object pulled from the object pool and it allows the game to carry on running instead of crashing

--- MinecraftClone/MinecraftClone/ObjectPool.h
+++ MinecraftClone/MinecraftClone/ObjectPool.h
@@ -10,35 +10,28 @@ template <class Object>
 struct ObjectInPool : private NonCopyable
 {
 	ObjectInPool()
-		: object(),
-		nextAvailableObject()
+		: object()
 	{}
 	ObjectInPool(ObjectInPool&& orig) noexcept
-		: object(std::move(orig.object)),
-		nextAvailableObject(orig.nextAvailableObject)
-	{
-		orig.nextAvailableObject = nullptr;
-	}
+		: object(std::move(orig.object))
+	{}
 	ObjectInPool& operator=(ObjectInPool&& orig) noexcept
 	{
 		object = std::move(orig.object);
-		nextAvailableObject = orig.nextAvailableObject;
-		orig.nextAvailableObject = nullptr;
 
 		return *this;
 	}
 	virtual ~ObjectInPool() {}
 
 	Object object;
-	ObjectInPool* nextAvailableObject;
 };
 
 //External Use - Object Pool
 template <class Object, class ObjectPool>
 struct ObjectFromPool : private NonCopyable
 {
-	ObjectFromPool(Object& object) 
-		: object(&object)
+	ObjectFromPool(Object* object) 
+		: object(object)
 	{}
 	virtual ~ObjectFromPool() {}
 	ObjectFromPool(ObjectFromPool&& orig) noexcept
@@ -63,8 +56,7 @@ class ObjectPool : private NonCopyable, private NonMovable
 {
 public:
 	ObjectPool()
-		: m_objectPool(),
-		m_nextAvailableObject(nullptr)
+		: m_objectPool()
 	{
 		//Added a little bit more than neccessary due to how the inifinite map generates
 		int x = Utilities::VISIBILITY_DISTANCE / Utilities::CHUNK_WIDTH;
@@ -72,36 +64,23 @@ public:
 		int y = Utilities::VISIBILITY_DISTANCE / Utilities::CHUNK_DEPTH;
 		y += y += 2;
 
-		m_objectPool.resize(size_t((x * y)));
-
-		for (int i = 0; i < static_cast<int>(m_objectPool.size()) - 1; ++i)
-		{
-			m_objectPool[i].nextAvailableObject = &m_objectPool[i + 1];
-		}
-
-		m_nextAvailableObject = &m_objectPool.front();
-		m_objectPool.back().nextAvailableObject = m_nextAvailableObject;
+		m_objectPool.resize(size_t(x * y));
 	}
 	virtual ~ObjectPool() {}
 
 protected:
-	ObjectInPool<Object>& getNextAvailableObject()
-	{
-		int iterationCount = 0;
-		bool validChunkFound = false;
-
-		assert(m_nextAvailableObject);
-		while (m_nextAvailableObject->object.isInUse())
+	Object* getNextAvailableObject()
+	{		
+		for (auto& objectInPool : m_objectPool)
 		{
-			assert(m_nextAvailableObject->nextAvailableObject);
-			m_nextAvailableObject = m_nextAvailableObject->nextAvailableObject;
-
-			assert(++iterationCount && iterationCount <= m_objectPool.size());
+			if (!objectInPool.object.isInUse())
+			{
+				return &objectInPool.object;
+			}
 		}
 
-		return *m_nextAvailableObject;
+		return nullptr;
 	}
 private:
 	std::vector<ObjectInPool<Object>> m_objectPool;
-	ObjectInPool<Object>* m_nextAvailableObject;
 };
