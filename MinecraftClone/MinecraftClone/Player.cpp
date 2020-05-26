90044e284623f2b24da836cfc33430b2b9f0be9f
Ryan Swann <RyanSwann1992@gmail.com>
2020-05-26 20:02:52 +0100

    Created Player class

---
+++ MinecraftClone/MinecraftClone/Player.cpp
@@ -0,0 +1,101 @@
+#include "Player.h"
+#include <SFML/Graphics.hpp>
+
+namespace 
+{
+	constexpr glm::vec3 STARTING_PLAYER_POSITION{ 0.0f, 100.0f, 0.0f };
+	constexpr float WALKING_MOVEMENT_SPEED = 15.0f;
+}
+
+//Camera
+Camera::Camera()
+	: FOV(45.0f),
+	sensitivity(0.1f),
+	nearPlaneDistance(0.1f),
+	farPlaneDistance(1750.f),
+	front(),
+	right(),
+	up(),
+	yaw(-90.0f),
+	pitch(0.0f)
+{
+	front = { 0.0f, -1.0f, 0.0f };
+	right = glm::normalize(glm::cross({ 0.0f, 1.0f, 0.0f }, front));
+	up = { 0.0f, 1.0f, 0.0f };
+}
+
+void Camera::move(const sf::Window& window)
+{
+	yaw += (sf::Mouse::getPosition(window).x - static_cast<int>(window.getSize().x / 2)) * sensitivity;
+	pitch += (static_cast<int>(window.getSize().y / 2) - sf::Mouse::getPosition(window).y) * sensitivity;
+	
+	//Reset mouse position to centre of screen
+	sf::Mouse::setPosition(sf::Vector2i(window.getSize().x / 2, window.getSize().y / 2), window);
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 v = {
+		glm::cos(glm::radians(yaw)) * glm::cos(glm::radians(pitch)),
+		glm::sin(glm::radians(pitch)),
+		glm::sin(glm::radians(yaw)) * glm::cos(glm::radians(pitch)) };
+	front = glm::normalize(v);
+}
+
+//Player
+Player::Player()
+	: m_camera(),
+	m_position(STARTING_PLAYER_POSITION),
+	m_movementSpeed(WALKING_MOVEMENT_SPEED)
+{}
+
+const glm::vec3& Player::getPosition() const
+{
+	return m_position;
+}
+
+const Camera& Player::getCamera() const
+{
+	return m_camera;
+}
+
+void Player::reset()
+{
+	m_position = STARTING_PLAYER_POSITION;
+}
+
+void Player::moveCamera(const sf::Window& window)
+{
+	m_camera.move(window);
+}
+
+void Player::move(float deltaTime, std::mutex& playerMutex)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) ||
+		sf::Keyboard::isKeyPressed(sf::Keyboard::S) ||
+		sf::Keyboard::isKeyPressed(sf::Keyboard::A) ||
+		sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			m_position -= glm::normalize(glm::cross(m_camera.front, m_camera.up)) * m_movementSpeed * deltaTime;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			m_position += glm::normalize(glm::cross(m_camera.front, m_camera.up)) * m_movementSpeed * deltaTime;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			m_position += m_movementSpeed * m_camera.front * deltaTime;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			m_position -= m_movementSpeed * m_camera.front * deltaTime;
+		}
+
+		playerMutex.unlock();
+	}
+}
---
+++ MinecraftClone/MinecraftClone/Player.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "NonCopyable.h"
+#include "NonMovable.h"
+#include "glad.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include <mutex>
+#include <SFML/Graphics.hpp>
+
+struct Camera : private NonCopyable, private NonMovable
+{
+	Camera();
+
+	void move(const sf::Window& window);
+
+	const float FOV;
+	const float sensitivity;
+	const float nearPlaneDistance;
+	const float farPlaneDistance;
+	glm::vec3 front;
+	glm::vec3 right;
+	glm::vec3 up;
+	float yaw;
+	float pitch;
+};
+
+class Player : private NonCopyable, private NonMovable
+{
+public:
+	Player();
+	
+	const glm::vec3& getPosition() const;
+	const Camera& getCamera() const;
+
+	void reset();
+	void moveCamera(const sf::Window& window);
+	void move(float deltaTime, std::mutex& playerMutex);
+
+private:
+	Camera m_camera;
+	glm::vec3 m_position;
+	float m_movementSpeed;
+};
