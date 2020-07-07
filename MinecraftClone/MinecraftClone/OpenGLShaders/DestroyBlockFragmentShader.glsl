b5bee790977e7abe10e53098eec28bdc14d1b3d7
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-07 19:52:07 +0100

    DestroyBlockVisual new shader

---
+++ MinecraftClone/MinecraftClone/OpenGLShaders/DestroyBlockFragmentShader.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+
+out vec4 color;
+uniform sampler2D uTexture;
+
+in vec2 vTextCoord;
+
+void main()
+{
+	color = texture(uTexture, vTextCoord);
+	if (color == vec4(0.0, 0.0, 0.0, 1.0))
+	{
+		discard;
+	}
+};
