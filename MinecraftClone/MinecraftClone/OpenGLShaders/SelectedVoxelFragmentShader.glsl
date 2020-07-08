79e801f27ee0178eeacb080ea8e7cb477bc83a49
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-08 01:27:36 +0100

    SelectedVoxel Shader
    Temporary shader implementation

---
+++ MinecraftClone/MinecraftClone/OpenGLShaders/SelectedVoxelFragmentShader.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+
+out vec4 color;
+uniform sampler2D uTexture;
+
+in vec2 vTextCoord;
+
+void main()
+{
+	color = texture(uTexture, vTextCoord);
+	if (color.a == 0)
+	{
+		discard;
+	}
+};
