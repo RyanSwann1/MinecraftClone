5634d61a09e158e6a0cf080b5ac6626d2d60dfc0
Ryan Swann <RyanSwann1992@gmail.com>
2020-07-18 17:59:50 +0100

    Edited mipmap settings

--- MinecraftClone/MinecraftClone/TextureArray.cpp
+++ MinecraftClone/MinecraftClone/TextureArray.cpp
@@ -43,7 +43,8 @@ namespace
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.getSize().x, image.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, image.getPixelsPtr());
 		glGenerateMipmap(GL_TEXTURE_2D);
 
@@ -127,6 +128,7 @@ std::unique_ptr<TextureArray> TextureArray::create()
 	glGenTextures(1, &textureArrayID);
 
 	glBindTexture(GL_TEXTURE_2D_ARRAY, textureArrayID);
+
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER,
 		GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
